4fd1df1faaa95717050ec0c6bbb4921e8694a86a
Friscel <182411462+Friscel@users.noreply.github.com>
2025-03-29 03:20:53 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketbooking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
