9e46238547acf1e13f4bbc8c48ac1f6fafac945d
Friscel <182411462+Friscel@users.noreply.github.com>
2025-03-29 10:54:51 +0800

    updating variable name

--- BusinessDataLogicTicketBooking/BookingProcess.cs
+++ BusinessDataLogicTicketBooking/BookingProcess.cs
@@ -8,31 +8,31 @@ namespace BusinessDataLogicTicketBooking
 {
     public class BookingProcess
     {
-        public static string[] _movies = { "Avengers: Endgame", "Inception", "Interstellar", "Joker" };
-        public static int[] _availableTickets = { 55, 40, 30, 20 };
-        public static int[] _bookedTickets = { 0, 0, 0, 0 };
+        public static string[] movies = { "Avengers: Endgame", "Inception", "Interstellar", "Joker" };
+        public static int[] availableTickets = { 55, 40, 30, 20 };
+        public static int[] bookedTickets = { 0, 0, 0, 0 };
         public static string[] GetMovies()
         {
-            return _movies;
+            return movies;
         }
         public static int[] GetAvailableTickets()
         {
-            return _availableTickets;
+            return availableTickets;
         }
         // Method to update tickets based on action
         public static bool UpdateTickets(Actions action, int movieChoice, int amount)
         {
-            if (movieChoice < 0 || movieChoice >= _movies.Length)
+            if (movieChoice < 0 || movieChoice >= movies.Length)
             {
                 return false;
             }
 
             if (action == Actions.BookTicket)
             {
-                if (_availableTickets[movieChoice] >= amount)
+                if (availableTickets[movieChoice] >= amount)
                 {
-                    _availableTickets[movieChoice] -= amount;
-                    _bookedTickets[movieChoice] += amount;
+                    availableTickets[movieChoice] -= amount;
+                    bookedTickets[movieChoice] += amount;
                     return true;
                 }
                 return false;
@@ -40,10 +40,10 @@ namespace BusinessDataLogicTicketBooking
 
             if (action == Actions.CancelTicket)
             {
-                if (_bookedTickets[movieChoice] >= amount)
+                if (bookedTickets[movieChoice] >= amount)
                 {
-                    _availableTickets[movieChoice] += amount;
-                    _bookedTickets[movieChoice] -= amount;
+                    availableTickets[movieChoice] += amount;
+                    bookedTickets[movieChoice] -= amount;
                     return true;
                 }
                 return false;
@@ -54,30 +54,30 @@ namespace BusinessDataLogicTicketBooking
         // Method to check ticket availability
         public static bool CheckTicketAvailability(int movieChoice, int amount)
         {
-            if (movieChoice < 0 || movieChoice >= _movies.Length || amount <= 0)
+            if (movieChoice < 0 || movieChoice >= movies.Length || amount <= 0)
             {
                 return false;
             }
 
-            return _availableTickets[movieChoice] >= amount;
+            return availableTickets[movieChoice] >= amount;
         }
         // Method to get current available tickets for a movie
         public static int GetAvailableTicketsForMovie(int movieChoice)
         {
-            if (movieChoice < 0 || movieChoice >= _movies.Length)
+            if (movieChoice < 0 || movieChoice >= movies.Length)
             {
                 return -1;
             }
-            return _availableTickets[movieChoice];
+            return availableTickets[movieChoice];
         }
         // Method to get booked tickets for a movie
         public static int GetBookedTicketsForMovie(int movieChoice)
         {
-            if (movieChoice < 0 || movieChoice >= _movies.Length)
+            if (movieChoice < 0 || movieChoice >= movies.Length)
             {
                 return -1;
             }
-            return _bookedTickets[movieChoice];
+            return bookedTickets[movieChoice];
         }
     }
 }
